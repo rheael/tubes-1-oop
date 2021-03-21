4add8209a8a687815c1f53c9a5d4c51996ce5c93
Ruhiyahfw <faradishi@gmail.com>
2021-03-21 19:57:08 +0700

    engimon-belum final

---
+++ Engimon.cpp
@@ -0,0 +1,489 @@
+#include "Engimon.hpp"
+
+#include <iostream>
+using namespace std;
+
+// -------------- BASE CLASS : ENGIMON ------------------
+Engimon::Engimon(){
+    this->name = "ZZZ";
+    this->parent1name = "XXX";
+    this->parent2name = "YYY";
+    this->ability = new Skill[4];
+    this->level = 0;
+    this->exp = 0;
+    this->cumulativeExp = 0;
+    this->numAbility = 0;
+}
+
+Engimon::Engimon(string nama){
+    this->name = nama;
+    this->parent1name = "XXX";
+    this->parent2name = "YYY";
+    this->ability = new Skill[4];
+    this->level = 0;
+    this->exp = 0;
+    this->cumulativeExp = 0;
+    this->numAbility = 0;
+}
+
+Engimon::Engimon(string nama, string napar1, string napar2){
+    this->name = nama;
+    this->parent1name = napar1;
+    this->parent2name = napar2;
+    this->ability = new Skill[4];
+    this->level = 0;
+    this->exp = 0;
+    this->cumulativeExp = 0;
+    this->numAbility = 0;
+}
+
+Engimon::Engimon(const Engimon& other){
+    this->name = other.name;
+    this->parent1name = other.parent1name;
+    this->parent2name = other.parent2name;
+    this->ability = new Skill[4];
+    for (int i = 0; i < 4; i++){
+        this->ability[i] = other.ability[i];
+    }
+    this->level = other.level;
+    this->exp = other.exp;
+    this->cumulativeExp = other.cumulativeExp;
+    this->numAbility = other.numAbility;
+}
+
+Engimon& Engimon::operator<<(const Skill& skil){
+    if (this->numAbility <= 4 && this->punyaSkill(skil)){
+        this->ability[numAbility] = skil;
+    }
+    return *this;
+}
+
+Engimon::~Engimon(){
+    delete [] this->ability;
+}
+
+string Engimon::getName(){
+    return this->name;
+}
+
+string Engimon::getParent1Name(){
+    return this->parent1name;
+}
+
+string Engimon::getParent2Name(){
+    return this->parent2name;
+}
+
+//string Engimon::getParent1Species(){};
+//string Engimon::getParent2Species(){};
+//void Engimon::talk(){};
+//bool Engimon::punyaElemen(string){};
+
+void Engimon::setName(string nama){
+    this->name = nama;
+}
+
+void Engimon::levelUp(){
+    while (this->exp > 100){
+        this->level++;
+        this->exp = this->exp - 100;
+    }
+}
+
+void Engimon::addExp(int tambahan){
+    this->exp = this->exp+tambahan;
+    this->cumulativeExp = this->cumulativeExp+tambahan;
+}
+
+void Engimon::die(){ 
+    if (this->cumulativeExp > maxExp){
+        this->~Engimon();
+    }
+}
+
+void Engimon::showData(){
+    cout << "Level: "<<this->level << endl;
+    cout << "Exp: " << this->exp << endl;
+    cout << "Cumulative Exp: "<<this->cumulativeExp << endl;
+    cout << "Skills: "; // cetak skill
+    cout << endl;
+    cout << "Parent's name: " << this->parent1name << ", "<<this->parent2name << endl;
+}
+
+bool Engimon::punyaSkill(Skill s){
+    bool found = false;
+    int i = 0;
+    while (!found && i < this->numAbility){
+        if (this->ability[i].getNama() == s.getNama()){
+            found = true;
+        }
+        i++;
+    }
+    return found;
+}
+
+// -------DERIVED CLASS : FIRE ENGIMONS ----------------
+Skill a("Blaze");
+const Skill Charmamon::uniqueSkill = a;
+const string Charmamon::parentSpecies = "Charmamon";
+
+Charmamon::Charmamon(): Engimon(){
+    this->ability[0] = uniqueSkill;
+}
+
+Charmamon::Charmamon(string nama):Engimon(nama){
+    this->ability[0] = uniqueSkill;
+}
+
+Charmamon::Charmamon(string nama, string napar1, string napar2):Engimon(nama, napar1, napar2){
+    this->ability[0] = uniqueSkill;
+}
+
+Skill Charmamon::getuniqueSkill(){
+    return uniqueSkill;
+}
+
+string Charmamon::getParent1Species(){
+    return parentSpecies;
+}
+
+string Charmamon::getParent2Species(){
+    return parentSpecies;
+}
+
+void Charmamon::showData(){
+    cout << "Name: " << this->name << endl;
+    cout << "Species: Charmamon" << endl;
+    cout << "Element: Fire" << endl;
+    Engimon::showData();
+    cout << "Parent's species: Charmamon" << endl;
+}
+
+void Charmamon::talk(){
+    cout << "[" << this->name << "]: Chaaa!! Let's burn everything!!" << endl;
+}
+
+bool Charmamon::punyaElemen(string elem){
+    return (elem == "Fire");
+}
+
+// -------DERIVED CLASS : WATER ENGIMONS ----------------
+Skill b("Torrent");
+const Skill Squirtlmon::uniqueSkill = b;
+const string Squirtlmon::parentSpecies = "Squirtlmon";
+
+Squirtlmon::Squirtlmon(): Engimon(){
+    this->ability[0] = uniqueSkill;
+}
+
+Squirtlmon::Squirtlmon(string nama):Engimon(nama){
+    this->ability[0] = uniqueSkill;
+}
+
+Squirtlmon::Squirtlmon(string nama, string napar1, string napar2):Engimon(nama, napar1, napar2){
+    this->ability[0] = uniqueSkill;
+}
+
+string Squirtlmon::getParent1Species(){
+    return parentSpecies;
+}
+
+string Squirtlmon::getParent2Species(){
+    return parentSpecies;
+}
+
+void Squirtlmon::showData(){
+    cout << "Name: " << this->name << endl;
+    cout << "Species: Squirtlmon" << endl;
+    cout << "Element: Water" << endl;
+    Engimon::showData();
+    cout << "Parent's species: Squirtlmon" << endl;
+}
+
+void Squirtlmon::talk(){
+    cout << "[" << this->name << "]: Squii!! Splash splash!!" << endl;
+}
+
+bool Squirtlmon::punyaElemen(string elem){
+    return (elem == "Water");
+}
+
+// -------DERIVED CLASS : ELECTRIC ENGIMONS ----------------
+
+Skill c("Static");
+const Skill Pikamon::uniqueSkill = c;
+const string Pikamon::parentSpecies = "Pikamon";
+
+Pikamon::Pikamon(): Engimon(){
+    this->ability[0] = uniqueSkill;
+}
+
+Pikamon::Pikamon(string nama):Engimon(nama){
+    this->ability[0] = uniqueSkill;
+}
+
+Pikamon::Pikamon(string nama, string napar1, string napar2):Engimon(nama, napar1, napar2){
+    this->ability[0] = uniqueSkill;
+}
+
+string Pikamon::getParent1Species(){
+    return parentSpecies;
+}
+
+string Pikamon::getParent2Species(){
+    return parentSpecies;
+}
+
+void Pikamon::showData(){
+    cout << "Name: " << this->name << endl;
+    cout << "Species: Pikamon" << endl;
+    cout << "Element: Electric" << endl;
+    Engimon::showData();
+    cout << "Parent's species: Pikamon" << endl;
+}
+
+void Pikamon::talk(){
+    cout << "[" << this->name << "]: Pika Pika!!! Bzzzz" << endl;
+}
+
+bool Pikamon::punyaElemen(string elem){
+    return (elem == "Electric");
+}
+
+// -------DERIVED CLASS : GROUND ENGIMONS ----------------
+Skill d("Sand Veil");
+const Skill Rumblemon::uniqueSkill = d;
+const string Rumblemon::parentSpecies = "Rumblemon";
+
+Rumblemon::Rumblemon(): Engimon(){
+    this->ability[0] = uniqueSkill;
+}
+
+Rumblemon::Rumblemon(string nama):Engimon(nama){
+    this->ability[0] = uniqueSkill;
+}
+
+Rumblemon::Rumblemon(string nama, string napar1, string napar2):Engimon(nama, napar1, napar2){
+    this->ability[0] = uniqueSkill;
+}
+
+string Rumblemon::getParent1Species(){
+    return parentSpecies;
+}
+
+string Rumblemon::getParent2Species(){
+    return parentSpecies;
+}
+
+void Rumblemon::showData(){
+    cout << "Name: " << this->name << endl;
+    cout << "Species: Rumblemon" << endl;
+    cout << "Element: Ground" << endl;
+    Engimon::showData();
+    cout << "Parent's species: Rumblemon" << endl;
+}
+
+void Rumblemon::talk(){
+     cout << "[" << this->name << "]: Rumble, rumble, attack!!!" << endl;
+}
+
+bool Rumblemon::punyaElemen(string elem){
+    return (elem == "Ground");
+}
+
+// -------DERIVED CLASS : ICE ENGIMONS ----------------
+Skill e("Ice body");
+const Skill Snommon::uniqueSkill = e;
+const string Snommon::parentSpecies = "Snommon";
+
+Snommon::Snommon(): Engimon(){
+    this->ability[0] = uniqueSkill;
+}
+
+Snommon::Snommon(string nama):Engimon(nama){
+    this->ability[0] = uniqueSkill;
+}
+
+Snommon::Snommon(string nama, string napar1, string napar2):Engimon(nama, napar1, napar2){
+    this->ability[0] = uniqueSkill;
+}
+
+string Snommon::getParent1Species(){
+    return parentSpecies;
+}
+
+string Snommon::getParent2Species(){
+    return parentSpecies;
+}
+
+void Snommon::showData(){
+    cout << "Name: " << this->name << endl;
+    cout << "Species: Snommon" << endl;
+    cout << "Element: Ice" << endl;
+    Engimon::showData();
+    cout << "Parent's species: Snommon" << endl;
+}
+
+void Snommon::talk(){
+     cout << "[" << this->name << "]: Snow snowwy weee!!!" << endl;
+}
+
+bool Snommon::punyaElemen(string elem){
+    return (elem == "Ice");
+}
+
+// -------DERIVED CLASS : ELECTROFIRE ENGIMONS ----------------
+Skill f("Levitate");
+const Skill Rotomon::uniqueSkill = f;
+
+Rotomon::Rotomon(): Engimon(){
+    this->ability[0] = uniqueSkill;
+    this->parent1Species = "Charmamon";
+    this->parent2Species = "Pikamon";
+}
+
+Rotomon::Rotomon(string nama):Engimon(nama){
+    this->ability[0] = uniqueSkill;
+    this->parent1Species = "Charmamon";
+    this->parent2Species = "Pikamon";
+}
+
+Rotomon::Rotomon(string nama, string napar1, string napar2):Engimon(nama, napar1, napar2){
+    this->ability[0] = uniqueSkill;
+    this->parent1Species = "Charmamon";
+    this->parent2Species = "Pikamon";
+}
+
+Rotomon::Rotomon(string nama, string napar1, string napar2, string spec1, string spec2):Engimon(nama, napar1, napar2){
+    this->ability[0] = uniqueSkill;
+    this->parent1Species = spec1;
+    this->parent2Species = spec2;
+}
+
+string Rotomon::getParent1Species(){
+    return parent1Species;
+}
+
+string Rotomon::getParent2Species(){
+    return parent2Species;
+}
+
+void Rotomon::showData(){
+    cout << "Name: " << this->name << endl;
+    cout << "Species: Rotomon" << endl;
+    cout << "Element: Fire, Electric" << endl;
+    Engimon::showData();
+    cout << "Parent's species: Rotomon" << endl;
+}
+
+void Rotomon::talk(){
+     cout << "[" << this->name << "]: Rotom!! Kaboom!! " << endl;
+}
+
+bool Rotomon::punyaElemen(string elem){
+    return (elem == "Fire" || elem == "Electric");
+}
+
+// -------DERIVED CLASS : ICE/WATER ENGIMONS ----------------
+Skill g("Thick fat");
+const Skill Sealmon::uniqueSkill = g;
+
+Sealmon::Sealmon(): Engimon(){
+    this->ability[0] = uniqueSkill;
+    this->parent1Species = "Snommon";
+    this->parent2Species = "Squirtlmon";
+}
+
+Sealmon::Sealmon(string nama):Engimon(nama){
+    this->ability[0] = uniqueSkill;
+    this->parent1Species = "Snommon";
+    this->parent2Species = "Squirtlmon";
+}
+
+Sealmon::Sealmon(string nama, string napar1, string napar2):Engimon(nama, napar1, napar2){
+    this->ability[0] = uniqueSkill;
+    this->parent1Species = "Snommon";
+    this->parent2Species = "Squirtlmon";
+}
+
+Sealmon::Sealmon(string nama, string napar1, string napar2, string spec1, string spec2):Engimon(nama, napar1, napar2){
+    this->ability[0] = uniqueSkill;
+    this->parent1Species = spec1;
+    this->parent2Species = spec2;
+}
+
+string Sealmon::getParent1Species(){
+    return parent1Species;
+}
+
+string Sealmon::getParent2Species(){
+    return parent2Species;
+}
+
+void Sealmon::showData(){
+    cout << "Name: " << this->name << endl;
+    cout << "Species: Sealmon" << endl;
+    cout << "Element: Fire, Electric" << endl;
+    Engimon::showData();
+    cout << "Parent's species: Sealmon" << endl;
+}
+
+void Sealmon::talk(){
+     cout << "[" << this->name << "]: Aye, aye, sealmon!!" << endl;
+}
+
+bool Sealmon::punyaElemen(string elem){
+    return (elem == "Water" || elem == "Ice");
+}
+
+// -------DERIVED CLASS : WATER/GROUND ENGIMONS ----------------
+Skill h("Sticky hold");
+const Skill Gastromon::uniqueSkill = h;
+
+Gastromon::Gastromon(): Engimon(){
+    this->ability[0] = uniqueSkill;
+    this->parent1Species = "Snommon";
+    this->parent2Species = "Rumblemon";
+}
+
+Gastromon::Gastromon(string nama):Engimon(nama){
+    this->ability[0] = uniqueSkill;
+    this->parent1Species = "Snommon";
+    this->parent2Species = "Rumblemon";
+}
+
+Gastromon::Gastromon(string nama, string napar1, string napar2):Engimon(nama, napar1, napar2){
+    this->ability[0] = uniqueSkill;
+    this->parent1Species = "Snommon";
+    this->parent2Species = "Rumblemon";
+}
+
+Gastromon::Gastromon(string nama, string napar1, string napar2, string spec1, string spec2):Engimon(nama, napar1, napar2){
+    this->ability[0] = uniqueSkill;
+    this->parent1Species = spec1;
+    this->parent2Species = spec2;
+}
+
+string Gastromon::getParent1Species(){
+    return parent1Species;
+}
+
+string Gastromon::getParent2Species(){
+    return parent2Species;
+}
+
+void Gastromon::showData(){
+    cout << "Name: " << this->name << endl;
+    cout << "Species: Gastromon" << endl;
+    cout << "Element: Water, Ground" << endl;
+    Engimon::showData();
+    cout << "Parent's species: Gastromon" << endl;
+}
+
+void Gastromon::talk(){
+     cout << "[" << this->name << "]: Gastro, gastro, let's go!! " << endl;
+}
+
+bool Gastromon::punyaElemen(string elem){
+    return (elem == "Water" || elem =="Ground");
+}
---
+++ Engimon.hpp
@@ -0,0 +1,216 @@
+#include "Skill.cpp"  // ini butuh kelas skill :D
+#include <iostream>
+using namespace std;
+
+class Engimon{
+    private:
+        int numAbility;
+    protected:
+        string name;
+        string parent1name;
+        string parent2name;
+        Skill* ability;
+        int level;
+        int exp;
+        int cumulativeExp;
+        static const int maxExp = 5000;
+    public:
+        // ctor, cctor, dtor
+        Engimon();
+        Engimon(string nama);
+        Engimon(string nama, string napar1, string napar2);
+        Engimon(const Engimon&);
+        virtual ~Engimon();
+        // getter, setter
+        string getName();
+        string getParent1Name();
+        string getParent2Name();
+        virtual string getParent1Species()=0;
+        virtual string getParent2Species()=0;
+        void setName(string);
+        //method overloading
+        Engimon& operator=(const Engimon&);
+        Engimon& operator<<(const Skill&);
+        // method lainnya
+        void levelUp();
+        void addExp(int);
+        void die(); // --------------------ini apakah iya?
+        virtual void showData();
+        virtual void talk()=0;
+        virtual bool punyaElemen(string)=0;
+        bool punyaSkill(Skill);
+};
+
+class Charmamon: public Engimon{
+    protected:
+        static const Skill uniqueSkill;
+        static const string parentSpecies;
+    public:
+        // ctor, cctor, dtor
+        Charmamon();
+        Charmamon(string nama);
+        Charmamon(string nama, string napar1, string napar2);
+        //Charmamon(const Engimon&);
+        //~Charmamon();
+        //getter, setter
+        Skill getuniqueSkill();
+        string getParent1Species();
+        string getParent2Species();
+        // method lain
+        void showData();
+        void talk();
+        bool punyaElemen(string);
+};
+
+class Squirtlmon: public Engimon{
+    protected:
+        static const Skill uniqueSkill;
+        static const string parentSpecies;
+    public:
+        // ctor, cctor, dtor
+        Squirtlmon();
+        Squirtlmon(string nama);
+        Squirtlmon(string nama, string napar1, string napar2);
+        //Squirtlmon(const Engimon&);
+        //~Squirtlmon();
+        //getter, setter
+        Skill getuniqueSkill();
+        string getParent1Species();
+        string getParent2Species();
+        // method lain
+        void showData();
+        void talk();
+        bool punyaElemen(string);
+};
+
+class Pikamon: public Engimon{
+    protected:
+        static const Skill uniqueSkill;
+        static const string parentSpecies;
+    public:
+        // ctor, cctor, dtor
+        Pikamon();
+        Pikamon(string nama);
+        Pikamon(string nama, string napar1, string napar2);
+        //Pikamon(const Engimon&);
+        //~Pikamon();
+        //getter, setter
+        Skill getuniqueSkill();
+        string getParent1Species();
+        string getParent2Species();
+        // method lain
+        void showData();
+        void talk();
+        bool punyaElemen(string);
+};
+
+class Rumblemon: public Engimon{
+    protected:
+        static const Skill uniqueSkill;
+        static const string parentSpecies;
+    public:
+        // ctor, cctor, dtor
+        Rumblemon();
+        Rumblemon(string nama);
+        Rumblemon(string nama, string napar1, string napar2);
+        //Rumblemon(const Engimon&);
+        //~Rumblemon();
+        //getter, setter
+        Skill getuniqueSkill();
+        string getParent1Species();
+        string getParent2Species();
+        // method lain
+        void showData();
+        void talk();
+        bool punyaElemen(string);
+};
+
+class Snommon: public Engimon{
+    protected:
+        static const Skill uniqueSkill;
+        static const string parentSpecies;
+    public:
+        // ctor, cctor, dtor
+        Snommon();
+        Snommon(string nama);
+        Snommon(string nama, string napar1, string napar2);
+        //Snommon(const Engimon&);
+        //~Snommon();
+        //getter, setter
+        Skill getuniqueSkill();
+        string getParent1Species();
+        string getParent2Species();
+        // method lain
+        void showData();
+        void talk();
+        bool punyaElemen(string);
+};
+
+class Rotomon: public Engimon{
+    protected:
+        static const Skill uniqueSkill;
+        string parent1Species;
+        string parent2Species;
+    public:
+        // ctor, cctor, dtor
+        Rotomon();
+        Rotomon(string nama);
+        Rotomon(string nama, string napar1, string napar2);
+        Rotomon(string nama, string napar1, string napar2, string spec1, string spec2);
+        //Rotomon(const Engimon&);
+        //~Rotomon();
+        //getter, setter
+        Skill getuniqueSkill();
+        string getParent1Species();
+        string getParent2Species();
+        // method lain
+        void showData();
+        void talk();
+        bool punyaElemen(string);
+};
+
+class Sealmon: public Engimon{
+    protected:
+        static const Skill uniqueSkill;
+        string parent1Species;
+        string parent2Species;
+    public:
+        // ctor, cctor, dtor
+        Sealmon();
+        Sealmon(string nama);
+        Sealmon(string nama, string napar1, string napar2);
+        Sealmon(string nama, string napar1, string napar2, string spec1, string spec2);
+        //Sealmon(const Engimon&);
+        //~Sealmon();
+        //getter, setter
+        Skill getuniqueSkill();
+        string getParent1Species();
+        string getParent2Species();
+        // method lain
+        void showData();
+        void talk();
+        bool punyaElemen(string);
+};
+
+class Gastromon: public Engimon{
+    protected:
+        static const Skill uniqueSkill;
+        string parent1Species;
+        string parent2Species;
+    public:
+        // ctor, cctor, dtor
+        Gastromon();
+        Gastromon(string nama);
+        Gastromon(string nama, string napar1, string napar2);
+        Gastromon(string nama, string napar1, string napar2, string spec1, string spec2);
+        //Gastromon(const Engimon&);
+        //~Gastromon();
+        //getter, setter
+        Skill getuniqueSkill();
+        string getParent1Species();
+        string getParent2Species();
+        // method lain
+        void showData();
+        void talk();
+        bool punyaElemen(string);
+};
